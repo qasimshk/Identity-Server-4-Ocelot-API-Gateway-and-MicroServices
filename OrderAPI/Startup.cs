f3056e6538f1afca1714c88b403a24cca54b7b9c
Muhammad Qasim <ins_sheikh@hotmail.com>
2018-12-04 14:04:02 +0000

    replace order project

--- OrderAPI/Startup.cs
+++ OrderAPI/Startup.cs
@@ -1,4 +1,4 @@
-using IdentityServer4.AccessTokenValidation;
+﻿using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
